0b9696bd25381230b0f1b07fe6f2b014ea05167b
marlontunchez@gmail.com <Marlon.1989>
2019-07-07 16:06:33 -0600

    nuevo

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ let cors= require("cors");
 let rusuario= require("./src/routes/rusuario.js");
 let rlogin= require("./src/routes/rlogin.js");
 
-let morgan = require("morgan");
+let morgan =  require("morgan");
 app.use(express.json());
 app.use(morgan("dev"));
 app.use('/api', rusuario);
 app.use('/api', rlogin);
 
-app.listen(port,()=>{
+app.listen(process.env.port,()=>{
 console.log("El servidor esta corriendo en el puerto: "+port);
 });
 module.exports = app;
