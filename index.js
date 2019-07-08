7fc314b7f54279cf0179c723fea09a222643bbcf
marlontunchez@gmail.com <Marlon.1989>
2019-07-08 09:30:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(morgan("dev"));
 app.use('/api', rusuario);
 app.use('/api', rlogin);
 
-app.listen(port,()=>{
+app.listen(process.env.port,()=>{
 console.log("El servidor esta corriendo en el puerto: "+port);
 });
 module.exports = app;
