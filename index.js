07746ac7fa641e7fe7e2fce2450797ef519442b4
marlontunchez@gmail.com <Marlon.1989>
2019-07-08 09:45:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express= require("express");
 let app = express();
-var port= process.env.port
+var port= process.env.PORT;
 let cors= require("cors");
 let rusuario= require("./src/routes/rusuario.js");
 let rlogin= require("./src/routes/rlogin.js");
