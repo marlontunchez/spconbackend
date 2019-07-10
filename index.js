e584a8e8a5664bb368db7a26736210130034b924
marlontunchez@gmail.com <Marlon.1989>
2019-07-10 17:57:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ let rusuario= require("./src/routes/rusuario.js");
 let rlogin= require("./src/routes/rlogin.js");
 let rcurso= require("./src/routes/rcurso.js");
 let morgan =  require("morgan");
-app.use(cors);
+
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors);
 app.use('/api', rusuario);
 app.use('/api', rlogin);
 app.use('/api', rcurso);
